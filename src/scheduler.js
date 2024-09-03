bd564ba42e24d94e44093fc094fa23435e5f35d1
code_akimbo <127673732+praveenraj-d@users.noreply.github.com>
2024-09-03 09:19:14 +0530

    Add files via upload

---
+++ src/scheduler.js
@@ -0,0 +1,28 @@
+const cron = require('node-cron');
+const { exec } = require('child_process');
+const path = require('path');
+
+const script = path.resolve(__dirname, './handlers/local.js');
+
+function runLocalScript() {
+  exec(`node ${script}`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error executing local.js: ${err}`);
+      return;
+    }
+    console.log(`Output: ${stdout}`);
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+    }
+  });
+}
+
+cron.schedule('0 8 15,28,30 * *', () => {
+  console.log('Running local.js at 8 AM...');
+  runLocalScript();
+});
+
+cron.schedule('0 20 15,28,30 * *', () => {
+  console.log('Running local.js at 8 PM...');
+  runLocalScript();
+});
